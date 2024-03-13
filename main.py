f9bf7370db09aaebb83cfb4e7baa6880a1ad2212
Lucas Araujo <lucasaraujo.contactme@gmail.com>
2024-03-13 14:07:58 -0300

    added labels TIMER, CHECKMARK and BUTTONS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -18,15 +18,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 #Create Window
-window = tkinter.Tk()
+window = tk.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+#Creating Label "Timer"
+title_label = tk.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 #Overlap Tomato Image on Window
-canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = tkinter.PhotoImage(file="./img/tomato.png")
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tk.PhotoImage(file="./img/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+#Creating Buttons
+start_button = tk.Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = tk.Button(text="Restart", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+#Creating checkmark
+checkmark = tk.Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=3)
 
 window.mainloop()
